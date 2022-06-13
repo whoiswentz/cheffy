b05c2039f6d9846b0c8f66aa89bf9930e37638d1
Vinicios <wentz.vinicios@gmail.com>
2022-06-12 22:48:47 -0300

    user namespace setup

--- src/dev/user.clj
+++ src/dev/user.clj
@@ -1,9 +1,26 @@
 (ns user
   (:require [io.pedestal.http :as http]))
 
-(http/start
-  (http/create-server {
-                       ::http/routes #{}
-                       ::http/type :jetty
-                       ::http/port 3000
-                       }))
+(defonce system-ref (atom nil))
+(defonce service-map {
+                  ::http/routes #{}
+                  ::http/type :jetty
+                  ::http/join? false
+                  ::http/port 3000
+                  })
+
+(defn start-dev []
+  (reset! system-ref
+          (-> service-map
+              (http/create-server)
+              (http/start)))
+  :started)
+
+(defn stop-dev []
+  (http/stop @system-ref)
+  :stopped)
+
+(defn restart-dev []
+  (stop-dev)
+  (start-dev)
+  :restarted)
