8acbc72ca0a9cc67605c5c43eced01bf7f79a70b
Vinicios Wentz <wentz.vinicios@gmail.com>
2026-07-29 10:40:43 -0300

    Add user dev namespace with Jetty server setup and update dependencies

---
+++ src/user.clj
@@ -0,0 +1,24 @@
+(ns user
+  (:require [io.pedestal.connector :as conn]
+            [io.pedestal.http.jetty :as jetty]))
+
+(defonce system-ref (atom nil))
+
+(defn start-dev
+  []
+  (reset! system-ref
+          (-> (conn/default-connector-map 3000)
+              (conn/with-routes #{})
+              (conn/with-default-interceptors
+                :allowed-origins ["http://localhost:8080"]
+                :mime-types      {"json" "application/json"
+                                  "edn"  "application/edn"})
+              (jetty/create-connector nil)
+              (conn/start!)))
+  :started)
+
+(defn stop-dev
+  []
+  (when @system-ref
+    (conn/stop! @system-ref)
+    :stopped))
