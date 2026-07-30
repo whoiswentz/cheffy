2c9a1dde2f2720d425851f8639cba3ba710f5376
Vinicios Wentz <wentz.vinicios@gmail.com>
2026-07-29 22:49:39 -0300

    Add basic recipe API routes to user dev server

--- src/user.clj
+++ src/user.clj
@@ -1,8 +1,26 @@
 (ns user
   (:require [io.pedestal.connector :as conn]
-            [io.pedestal.http.jetty :as jetty]))
+            [io.pedestal.http.jetty :as jetty]
+            [io.pedestal.http.route.definition.terse :as terse]))
 
 (defonce system-ref (atom nil))
+
+(defn list-recipes
+  [request]
+  {:status 200
+   :body   "list recipes"})
+
+(defn upsert-recipe
+  [request]
+  {:status 200
+   :body   "upsert recipes"})
+
+(def routes
+  (terse/terse-routes
+    [[:cheffy :http "localhost:8000"
+      ["/recipes" {:get  [:list-recipes `list-recipes]
+                   :post [:create-recipe `upsert-recipe]}
+       [":recipe-id" {:put [:update-recipe `upsert-recipe]}]]]]))
 
 (defn start-dev
   []
@@ -13,6 +31,7 @@
                 :allowed-origins ["http://localhost:8080"]
                 :mime-types      {"json" "application/json"
                                   "edn"  "application/edn"})
+              (conn/with-routes routes)
               (jetty/create-connector nil)
               (conn/start!)))
   :started)
