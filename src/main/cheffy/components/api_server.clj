f2e1b9b61d5ccb58c10b701ed0bc47d738d521d2
Vinicios Wentz <wentz.vinicios@gmail.com>
2026-08-03 20:39:55 -0300

    Fix ApiServer to use Pedestal 0.8 Connector API and inject configuration via service-map

---
+++ src/main/cheffy/components/api_server.clj
@@ -0,0 +1,25 @@
+(ns cheffy.components.api-server
+  (:require [cheffy.routes :as routes]
+            [com.stuartsierra.component :as component]
+            [io.pedestal.connector :as conn]
+            [io.pedestal.http.jetty :as jetty]))
+
+(defrecord ApiServer [service-map service]
+  component/Lifecycle
+  (start [component]
+    (let [port            (get service-map :port 3000)
+          allowed-origins (get service-map :allowed-origins ["http://localhost:8080"])
+          started-service (-> (conn/default-connector-map port)
+                              (conn/with-default-interceptors
+                                :allowed-origins allowed-origins
+                                :mime-types {"json" "application/json"
+                                             "edn"  "application/edn"})
+                              (conn/with-routes routes/routes)
+                              (jetty/create-connector nil)
+                              (conn/start!))]
+      (assoc component :service started-service)))
+
+  (stop [component]
+    (when service
+      (conn/stop! service))
+    (assoc component :service nil)))
