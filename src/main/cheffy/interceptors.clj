f7996ee954759fdd13c6320049c14b906e4e0882
Vinicios Wentz <wentz.vinicios@gmail.com>
2026-08-06 09:53:15 -0300

    Refactor interceptors to use pedestal functions and improve db injection

--- src/main/cheffy/interceptors.clj
+++ src/main/cheffy/interceptors.clj
@@ -1,6 +1,17 @@
-(ns cheffy.interceptors)
+(ns cheffy.interceptors
+  (:require [datomic.client.api :as d]
+            [io.pedestal.interceptor :refer [interceptor]]))
 
 (defn inject-database
   [database]
-  {:name  ::inject-database
-   :enter #(assoc-in % [:request :database] database)})
+  (interceptor
+    {:name  ::inject-database
+     :enter #(assoc-in % [:request :system/database] database)}))
+
+(def db-interceptor
+  (interceptor
+    {:name ::db-interceptor
+     :enter (fn [ctx]
+              (if-let [conn (get-in ctx [:request :system/database :conn])]
+                (assoc-in ctx [:request :db] (d/db conn))
+                ctx))}))
